4592cd8590622067090eed69f041306c55e3c000
hamzah <hamzahnafalah@gmail.com>
2025-12-04 11:28:20 +0700

    update instalasi dan satu sehat

--- src/Database/Seeders/AddDatabaseSeeder.php
+++ src/Database/Seeders/AddDatabaseSeeder.php
@@ -13,21 +13,6 @@ class AddDatabaseSeeder extends Seeder
     public function run(): void
     {
         echo "[DEBUG] Booting ".class_basename($this)."\n";
-        JobRequest::set([
-                'workspace_id' => '01kbk9kde1ap43xzmrw87qys8s',
-                'workspace_name' => 'Klinik Wellmed',
-                'product_label' => 'LITE',
-                'group_tenant_id' => 3,
-                'app_tenant_id' => 2,
-                'admin' => [
-                    "id"=> null,
-                    "name"=> "admin_wellmed",
-                    "username"=> "admin_wellmed",
-                    "email"=> "admin_wellmed@mail.com",
-                    "password"=> "password",
-                    "password_confirmation"=> "password"
-                ]
-                ]);
         try {
             config([
                 'laravel-feature.is_validate_restriction' => false,
@@ -38,7 +23,7 @@ class AddDatabaseSeeder extends Seeder
             $this->call([
                 AddNewTenantSeeder::class,                
                 InstallerSeeder::class,
-                // UserAdminSeeder::class,
+                UserAdminSeeder::class,
             ]);
         } catch (\Throwable $th) {
             throw $th;
