44795dd78f6a9ed2ed31d49f008b3a806407bf35
hamzah <hamzahnafalah@gmail.com>
2025-12-29 16:09:17 +0700

    update for satu sehat

--- src/Schemas/ModulePatient/VisitRegistration.php
+++ src/Schemas/ModulePatient/VisitRegistration.php
@@ -22,33 +22,33 @@ class VisitRegistration extends SchemasVisitRegistration implements ModulePatien
             $patient_model = $visit_registration_dto->patient_model ?? $visit_patient_model->patient ?? null;
             if (isset($patient_model)){
                 // $workspace = tenancy()->tenant->reference;
-                $medic_service = $visit_registration_model->medicService;
-                $room = $this->RoomModel()->where('medic_service_id',$medic_service->getKey())->whereNotNull("props->ihs_number")->first();
-                $form_payload = [
-                    'encounter_code' => $visit_registration_model->encounter_code,
-                    'status' => 'arrived',
-                    'class_code' => 'AMB',
-                    'patient_code' => $patient_model->prop_card_identity['ihs_number'] ?? null,
-                    'patient_name' => $patient_model->name,
-                    'participant' => [
-                        'attenders' => [
-                            [
-                                "participant_code" => "10006926841",
-                                "participant_name" => "dr. Yoga Yandika, Sp.A"
-                            ]
-                        ]
-                    ],
-                    // 'organization_code' => $workspace->integration['satu_sehat']['general']['ihs_number'] ?? null,
-                    'organization_code' => config('satu-sehat.organization_id'),
-                    'visit_code' => $visit_patient_model->visit_code ?? Str::orderedUuid()->toString(),
-                    'period' => $period = $visit_registration_model->created_at->format('Y-m-d H:i:s'),
-                    'status_history' => [
-                        'arrived' => $period
-                    ],
-                    'location_code' => $room->ihs_number ?? null,
-                    'location_name' => $room->ihs_name ?? $room->name." - ".$room->getKey()
-                ];
                 try {
+                    $medic_service = $visit_registration_model->medicService;
+                    $room = $this->RoomModel()->where('medic_service_id',$medic_service->getKey())->whereNotNull("props->ihs_number")->first();
+                    $form_payload = [
+                        'encounter_code' => $visit_registration_model->encounter_code,
+                        'status' => 'arrived',
+                        'class_code' => 'AMB',
+                        'patient_code' => $patient_model->prop_card_identity['ihs_number'] ?? null,
+                        'patient_name' => $patient_model->name,
+                        'participant' => [
+                            'attenders' => [
+                                [
+                                    "participant_code" => "10006926841",
+                                    "participant_name" => "dr. Yoga Yandika, Sp.A"
+                                ]
+                            ]
+                        ],
+                        // 'organization_code' => $workspace->integration['satu_sehat']['general']['ihs_number'] ?? null,
+                        'organization_code' => config('satu-sehat.organization_id'),
+                        'visit_code' => $visit_patient_model->visit_code ?? Str::orderedUuid()->toString(),
+                        'period' => $period = $visit_registration_model->created_at->format('Y-m-d H:i:s'),
+                        'status_history' => [
+                            'arrived' => $period
+                        ],
+                        'location_code' => $room->ihs_number ?? null,
+                        'location_name' => $room->ihs_name ?? $room->name." - ".$room->getKey()
+                    ];
                     $encounter_satu_sehat = $this->schemaContract('encounter_satu_sehat')->useAccessToSatuSehat()
                         ->prepareStoreEncounterSatuSehat(
                         $this->requestDTO(
